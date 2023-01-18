08f9033d372fc3cd2e9216402ac3e0c159e582e2
keenst <leo.siven@icloud.com>
2023-01-18 13:53:36 +0100

    Fully implemented data collection

--- Tonk3/AI/Rev3.cs
+++ Tonk3/AI/Rev3.cs
@@ -6,23 +6,25 @@ internal class Rev3 : Player
 {
     private List<Card> _cardsPlayed;
     private int _roundsPlayed;
+    private Card _lastCardSaid; // last card we said we played (could be lie)
+    private int _lastHandSize;  // hand size last round
     
-    private bool _calledBluff; // if this player called bluff last round (or this round depending on when accessing)
+    private bool _calledBluff;  // if this player called bluff last round (or this round depending on when accessing)
     
     private Card _prevOppCard;
     private int  _prevOppHandSize = 7;
 
     private readonly int[]
-        _oppBluffValues     = new int[15],
-        _oppHandSizeValues  = new int[20],
-        _myBluffValues      = new int[15],
-        _myHandSizeValues   = new int[20];
+        _bluffValues         = new int[15],
+        _handSizeValues      = new int[20],
+        _callValues          = new int[15],
+        _handSizeCallValues  = new int[20];
 
     private double[]
-        _oppBluffWeight     = new double[15],
-        _oppHandSizeWeight  = new double[20],
-        _myBluffWeight      = new double[15],
-        _myHandSizeWeight   = new double[20];
+        _bluffWeight         = new double[15],
+        _handSizeWeight      = new double[20],
+        _callWeight          = new double[15],
+        _handSizeCallWeight  = new double[20];
 
     public Rev3()
     {
@@ -33,23 +35,33 @@ internal class Rev3 : Player
     {
         Card card = new(cardValue, cardSuit);
 
+        // collect opponent bluff statistics
         if (_calledBluff && WasLastCallCorrect)
         {
             //if (_prevOppCard != null)
             // ^ is necessary?
             {
                 _cardsPlayed.Add(_prevOppCard);
-                _oppBluffValues[_prevOppCard.Value - 1]++;
-                _oppHandSizeWeight[_prevOppHandSize]++;
+                _bluffValues[_prevOppCard.Value]++;
+                _handSizeValues[_prevOppHandSize]++;
             }
+        }
+        
+        // collect opponent call statistics
+        if (WasLastCardCalled)
+        {
+            _callValues[_lastCardSaid.Value]++;
+            _handSizeCallValues[_lastHandSize]++;
         }
 
         _prevOppCard = card;
         _prevOppHandSize = Game.opponentHandSize(this);
         _calledBluff = false;
 
-        _oppBluffWeight     = Normalise(_oppBluffValues);
-        _oppHandSizeWeight  = Normalise(_oppHandSizeValues);
+        _bluffWeight        = Normalise(_bluffValues);
+        _handSizeWeight     = Normalise(_handSizeValues);
+        _callWeight         = Normalise(_callValues);
+        _handSizeCallWeight = Normalise(_handSizeValues);
 
         if (Hand.Count == 1 && Game.opponentHandSize(this) >= 3)
         {
